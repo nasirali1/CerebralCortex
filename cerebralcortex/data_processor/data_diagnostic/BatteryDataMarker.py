36ccb3f9ec2d9961d462227b9f130b3b441fe78c
ali <nasir.ali08@gmail.com>
2017-03-13 12:41:43 -0500

    added sensor battery marker algo

--- cerebralcortex/data_processor/data_diagnostic/BatteryDataMarker.py
+++ cerebralcortex/data_processor/data_diagnostic/BatteryDataMarker.py
@@ -11,8 +11,26 @@ def phoneBatteryMarker(window_data, window_size):
 
 
 
-def senseBatteryMarker():
-    pass
+def senseBatteryMarker(window_data, window_size):
+
+    sensor_battery_down = 0
+    sensor_battery_off = 0
+
+    for val in window_data:
+        #ADC value to voltage
+        #Values (Min=0 and Max=6) in battery voltage.
+        voltageValue = ( val / 4096) * 3 * 2
+
+        if voltageValue <= 2 & voltageValue>0:
+            ++sensor_battery_down
+
+    data_available = len(window_data)/window_size
+    available_battery = sensor_battery_down / window_size
+
+    if data_available < 0.05:
+        return "sensor-powered-off"
+    elif available_battery < 0.20:
+        return "sensor-battery-down"
 
 def motionSenseBatteryMarker():
     pass
