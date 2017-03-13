e7ab400ecd036b2f9223a1ddcd9278fa1ae48013
ali <nasir.ali08@gmail.com>
2017-03-13 11:34:23 -0500

    fixed a bug and added license info

--- cerebralcortex/data_processor/data_diagnostic/VarianceBasedDataQuality.py
+++ cerebralcortex/data_processor/data_diagnostic/VarianceBasedDataQuality.py
@@ -1,32 +1,64 @@
+# Copyright (c) 2016, MD2K Center of Excellence
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 import numpy as np
 
-def variance_based_data_quality(window_data):
+
+def variance_based_data_quality(window_data: list) -> str:
     """
     This method accepts one window at a time
     :param raw_sensor_data:
     """
-    #remove outliers from the window data
+    # remove outliers from the window data
     normal_values = outlier_detection(window_data)
 
-    #TO-DO: move all threshold values in config
+    # TO-DO: move all threshold values in config
     if np.var(normal_values) < 0.7:
         return "off-body"
 
     return "on-body"
 
 
-def outlier_detection(signal_values):
+def outlier_detection(signal_values: list) -> list:
+    """
+    removes outliers from a list
+    This algorithm is modified version of Chauvenet's_criterion (https://en.wikipedia.org/wiki/Chauvenet's_criterion)
+    :param signal_values:
+    :return:
+    """
     if not signal_values:
         return
 
     median = np.median(signal_values)
     standard_deviation = np.std(signal_values)
-    normal_values = ""
+    normal_values = list()
 
     for val in signal_values:
         if (abs(val) - median) < standard_deviation:
-            normal_values.extend(val)
+            normal_values.append(val)
 
     return normal_values
 
-variance_based_data_quality([1,2,3])
+
+variance_based_data_quality([1, 2, 3])
