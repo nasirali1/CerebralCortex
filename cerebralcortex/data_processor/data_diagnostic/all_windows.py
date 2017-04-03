4ad2fc8a84b430684f6cd93b071ae8168b5b63cf
ali <nasir.ali08@gmail.com>
2017-04-02 23:30:10 -0500

    adding a new windowing method

--- cerebralcortex/data_processor/data_diagnostic/all_windows.py
+++ cerebralcortex/data_processor/data_diagnostic/all_windows.py
@@ -37,8 +37,40 @@ class All_Windows:
         start_time = data[0].start_time
         end_time = data[len(data)-1].start_time
 
-        self.create_blank_windows(data, start_time, end_time);
+        self.create_blank_windows(data, window_size, start_time, end_time);
 
 
-    def create_blank_windows(self, start_time, end_time):
-        pass
+    def create_blank_windows(self, data, window_size, start_time, end_time):
+        time_delta = (end_time-start_time).total_seconds()
+        total_windows = math.floor(time_delta/window_size)
+        print(total_windows)
+        end_time2 = start_time
+        for i in range(total_windows):
+            start_time = end_time2
+            end_time2 = start_time + timedelta(seconds=window_size)
+
+            data2 = [i for i in data if start_time<= i.start_time and i.start_time <= end_time2]
+            if not data2:
+                print(str(start_time)+""+str(end_time2)+" NO DATA")
+            else:
+                print(str(start_time)+""+str(end_time2)+" - "+str(data2))
+            # for j in range(len(data)):
+            #     #print(j)
+            #     if start_time<= data[j].start_time and data[j].start_time <= end_time2:
+            #         print(str(start_time) +" - "+str(end_time2))
+            #         #data.pop(j)
+            #         #continue
+            #     print("IN "+str(start_time))
+            # print("out "+str(start_time))
+            # for j in self.simple_gen(data, start_time, end_time2):
+            #     print(j)
+            # print("out "+str(start_time))
+
+    def simple_gen(self,data, st, et):
+        for j in range(len(data)):
+            if st<= data[j].start_time and data[j].start_time <= et:
+                yield str(st) +" - "+str(et)
+            #print(st)
+            #print(str(len(data))+"yild "+str(j))
+            print("IN "+str(st))
+
