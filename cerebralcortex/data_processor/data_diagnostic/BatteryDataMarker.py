e56dd42939c857517bcbcce19bb513b0446cb63b
ali <nasir.ali08@gmail.com>
2017-04-04 11:24:42 -0500

    updated sensor battery data diagnostic algo

--- cerebralcortex/data_processor/data_diagnostic/BatteryDataMarker.py
+++ cerebralcortex/data_processor/data_diagnostic/BatteryDataMarker.py
@@ -21,67 +21,114 @@
 # CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+from typing import List
+import numpy as np
 
-def phoneBatteryMarker(battery_levels: list, window_size: int) -> str:
-    # TO-DO: if battery levels are empty for the whole window then check for the previous marked window
-    # and mark this window as the previous one
+from cerebralcortex.kernel.datatypes.datapoint import DataPoint
+from cerebralcortex.data_processor.signalprocessing.window import window
+from cerebralcortex.data_processor.data_diagnostic.util import *
 
+def battery_marker(datapoints: List[DataPoint], window_size: int, type: str) -> OrderedDict:
+    """
+    :param datapoints:
+    :param window_size: in seconds
+    :param type: acceptable types are: phone, autosense, motionsense
+    :return: OrderedDict representing [(st,et),[label],
+                                       (st,et),[label],
+                                        ...]
+    """
+    windowed_data = window(datapoints, window_size, True)
+    results = OrderedDict()
+    for key, data in windowed_data.items():
+        dp = []
+        for k in data:
+            dp.append(float(k.sample))
+
+        if type=="phone":
+            results[key] = phone_battery(dp)
+
+        elif type=="motionsense":
+            results[key] = phone_battery(dp)
+        elif type=="autosense":
+            results = phone_battery(dp)
+        else:
+            raise ValueError("Incorrect sensor type.")
+    merged_windows = merge_consective_windows(results)
+    return merged_windows
+
+def phone_battery(dp: List) -> str:
     """
 
-    :param battery_levels:
-    :param window_size:
+    :param dp:
     :return:
     """
-    if not battery_levels:
-        pass
+    if not dp:
+        return "phone-off"
+    else:
+        #get sampling rate and check if the current window has acceptable number of samples
+        """TO-DO"""
+        dp_sample_avg = np.mean(dp)
+        if dp_sample_avg<10:
+            return "phone-battery-down"
+    return  None
 
-    phone_battery_down = 0
-    phone_battery_charged = 0
-
-    for val in battery_levels:
-        if val < 10:
-            ++phone_battery_down
-        elif val > 50:
-            ++phone_battery_charged
-
-    battery_down = phone_battery_down / len(battery_levels)
-    battery_charged = phone_battery_charged / len(battery_levels)
-
-    if battery_down > 0.50:
-        return "phone-battery-down"
-    elif battery_charged > 0.80:
-        return "phone-powered-off"
-
-
-def autosenseBatteryMarker(battery_levels: list, window_size: int) -> str:
+def motionSenseBatteryMarker(dp: List) -> str:
     """
 
-    :param battery_levels:
-    :param window_size:
+    :param dp:
     :return:
     """
-    sensor_battery_down = 0
-    sensor_battery_charged = 0
+    if not dp:
+        return "motionsense-off"
+    else:
+        #get sampling rate and check if the current window has acceptable number of samples
+        """TO-DO"""
+        dp_sample_avg = np.mean(dp)
+        if dp_sample_avg<10:
+            return "motionsense-battery-down"
+    return None
 
-    for val in battery_levels:
-        # ADC value to voltage
+def autosense_battery(dp: List) -> str:
+    """
+
+    :param dp:
+    :return:
+    """
+    if not dp:
+        return "sensor-off"
+    else:
+        dp_sample_avg = np.mean(dp)
         # Values (Min=0 and Max=6) in battery voltage.
-        voltageValue = (val / 4096) * 3 * 2
-
-        if voltageValue <= 2:
-            ++sensor_battery_down
-        elif voltageValue > 2:
-            ++sensor_battery_charged
-
-    battery_down = sensor_battery_down / len(battery_levels)
-    battery_charged = sensor_battery_charged / len(battery_levels)
-
-    if battery_down > 0.50:
-        return "sensor-battery-down"
-    elif battery_charged > 0.80:
-        return "sensor-powered-off"
+        voltageValue = (dp_sample_avg / 4096) * 3 * 2
+        if voltageValue<0.5:
+            return "sensor-battery-down"
+    return None
 
 
-def motionSenseBatteryMarker():
-    # same as phone-battery-marker
-    pass
+def merge_consective_windows(data: OrderedDict) -> OrderedDict:
+    """
+    Merge two or more windows if the time difference between them is 0
+    :param data:
+    :return:
+    """
+    merged_windows = OrderedDict()
+    element = None
+    start = None
+    end = None
+    for key, val in data.items():
+        if element is None:
+            element = val
+            start = key[0]
+            end = key[1]
+        elif element == val:
+            element = val
+            end = key[1]
+        else:
+            merged_windows[(start, end)] = element
+            element = val
+            start = key[0]
+            end = key[1]
+    if val is not None:
+        merged_windows[(start, end)] = val
+
+    return merged_windows
