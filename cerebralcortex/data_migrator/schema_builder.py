0a76ad23b96096cd7716202bd9817577442a231d
ali <nasir.ali08@gmail.com>
2017-05-17 12:26:58 -0500

    fixed some bugs Updated test-cases added some methods to retrieve stream info

--- cerebralcortex/data_migrator/schema_builder.py
+++ cerebralcortex/data_migrator/schema_builder.py
@@ -25,7 +25,6 @@
 
 from cerebralcortex.kernel.schema_builder.execution_context import execution_context
 
-
 def get_execution_context(pm_algo_name: str, old_schema: dict) -> dict:
     """
     :param pm_algo_name:
